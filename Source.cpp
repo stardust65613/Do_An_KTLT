ab0bfe6e32913689b5720339ef8a1b9c9ee31205
Dang <gnadegreneyugn@gmail.com>
2024-04-19 05:09:29 +0700

    m

--- Source.cpp
+++ Source.cpp
@@ -3,15 +3,16 @@ int main(){
     SinhVien *sv = NULL;
     char *thu_muc = TaoThuMuc();
     int so_sv;
-    GetData(sv,so_sv);
-    cout << so_sv;
-    for(int i = 0; i < so_sv; i++){
-        TaoFileHTML(sv[i],thu_muc);
+    int flag = GetData(sv,so_sv);
+    if(flag == 0){
+        for(int i = 0; i < so_sv; i++){
+            TaoFileHTML(sv[i],thu_muc);
+        }
+        TaoFileCSS(thu_muc);
     }
     for(int i = 0; i < so_sv; i++){
-        ThuHoiBoNho(sv[i]);
+       ThuHoiBoNho(sv[i]);
     }
-    TaoFileCSS(thu_muc);
     delete [] thu_muc;
     delete [] sv;
     return 0;
